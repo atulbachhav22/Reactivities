d796d94afcd7b2f5fa3f9fa1e6f8a73d5ce00f3e
atulv <atulv@DESKTOP-L3L8RP0>
2021-08-23 22:38:30 -0400

    Add project files.

---
+++ API2/Controllers/Activities.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Activitiescontroller : ControllerBase
+    {
+        private DataContext _context;
+
+        public Activitiescontroller(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+
+        }
+
+    }
+}
